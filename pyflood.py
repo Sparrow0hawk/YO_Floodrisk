b051b209dacd6c93370008efb36800bce36be2f2
Salam697 <shoaib697@gmail.com>
2019-07-16 15:40:13 +0100

    Imported data from csv into list of tuples.

--- pyflood.py
+++ pyflood.py
@@ -7,9 +7,10 @@ with open('data/Flood impacts.csv') as csv_file:
     for row in csv_reader:
         if line_count == 0:
             label1, label2, label3 = row[1], row[2], row[5]
+            #print(label1, label2, label3)
         elif len(row[5]) > 10:
             gauge_name, rloi_id, location = row[1], row[2], row[5]
             longitude, latitude = location.split(',')[:2]
             locations.append((gauge_name, rloi_id, longitude, latitude))
-            print(locations[-1])
+            #print(locations[-1])
         line_count += 1

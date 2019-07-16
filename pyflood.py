7b6e553aefc6373ae46fe9f95d0a52c18168b55b
Salam697 <shoaib697@gmail.com>
2019-07-16 15:36:17 +0100

    Import and clean data from csv into list of tuples

---
+++ pyflood.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('data/Flood impacts.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    locations = []
+    for row in csv_reader:
+        if line_count == 0:
+            label1, label2, label3 = row[1], row[2], row[5]
+        elif len(row[5]) > 10:
+            gauge_name, rloi_id, location = row[1], row[2], row[5]
+            longitude, latitude = location.split(',')[:2]
+            locations.append((gauge_name, rloi_id, longitude, latitude))
+            print(locations[-1])
+        line_count += 1

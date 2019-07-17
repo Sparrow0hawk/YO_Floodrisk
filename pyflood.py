2d21dfb79138bfcb7635ab6b6f3f9dff1a7d699f
Salam697 <shoaib697@gmail.com>
2019-07-17 11:32:53 +0100

    Constructed geojson with social media links

--- pyflood.py
+++ pyflood.py
@@ -11,14 +11,28 @@ with open('data/Flood impacts.csv') as csv_file:
             #print(label1, label2, label3)
             pass
         elif len(row[5]) > 10:
-            gauge_name, rloi_id, location = row[1], row[2], row[5]
-            longitude, latitude = location.split(',')[:2]
+            gauge_name, rloi_id, location = row[1], int(row[2]), row[5]
+            co_ords = location.split(',')
+            co_ords = [value for value in co_ords if value !=""]
+            long_string, lat_string = tuple(co_ords)
+            #print(location, long_string, lat_string)
+            longitude, latitude = float(long_string), float(lat_string)
+            mailto = (f'mailto:reports@environmentagency.gov.uk'
+                    f'?subject=Flood report at {long_string},{lat_string}')
+            twitter = (f'https://twitter.com/intent/tweet?text='
+                        f'Flood report at {long_string},{lat_string}'
+                        f'&url=https%3A%2F%2Fwww.guardian.com')
+            facebook = (f'https://www.facebook.com/dialog/share?app_id=180444840287'
+                        f'&href=https%3A%2F%2Fwww.theguardian.com')
             features.append(
                 Feature(
                     geometry = Point((longitude, latitude)),
                     properties = {
                         'gauge_name' : gauge_name,
-                        'rloi_id' : rloi_id
+                        'rloi_id' : rloi_id,
+                        'mailto' : mailto,
+                        'twitter' : twitter,
+                        'facebook' : facebook,
                     }
                 )
             )
